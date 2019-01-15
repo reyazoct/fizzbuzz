c6de2f9ad4e48b8e4daa17cbdfe9fdd4659bcf85
Reyaz Ahmad <reyazoct@gmail.com>
2019-01-15 13:20:21 +0530

    add evaluation of fizzbuzz for multiple of three and five

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,5 @@
 def fizzbuzz(number)
+  return "fizzbuzz" if number % 15 == 0
   return "fizz" if number % 3 == 0
-  "buzz" if number % 5 == 0
+  return "buzz" if number % 5 == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,8 @@ describe "Fizz Buzz" do
   it "expects buzz given a multiple of five" do
     expect(fizzbuzz(10)).to eq "buzz"
   end
+  it "expects fizzbuzz given a multiple of both three and five" do
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
+  end
+
 end
