183302943d6dae7933c217adc0f2efece2572ab3
Reyaz Ahmad <reyazoct@gmail.com>
2019-01-15 13:15:09 +0530

    add evaluation of fizzbuzz for multiple of five

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,7 @@ describe "Fizz Buzz" do
   it "expects fizz given a multiple of three" do
     expect(fizzbuzz(3)).to eq "fizz"
   end
+  it "expects buzz given a multiple of five" do
+    expect(fizzbuzz(10)).to eq "buzz"
+  end
 end
